08e8a98bd613bfe352834dcca8a8198966bf7f21
senesuzun <senesuzun@gmail.com>
2024-04-05 16:59:53 +0200

    steps

---
+++ src/components/Hero.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+const Hero = () => {
+    return (
+        <div className="relative min-h-screen">
+            <Image className="lg:w-[580px] xl:w-[620px] h-auto absolute right-0 to-0 -z-10"
+                   src="/mainphoto.jpeg"
+            width={1000}
+            height={600}
+            alt="hero bg"/>
+            <Navbar/>
+
+            <div className="container h-[calc(100vh-120px)] grid items-center">
+                <div className="space-y-4 bg-[#ffffff98] w-fit p-4">
+                    <p className="uppercase font-medium">
+                        Wide options of choice
+                    </p>
+                    <h2 className="text-4xl sm:text-6xl font-bold">
+                        Best <span className="text-accent">Recipes</span>
+                    </h2>
+                    <p className="text-gray-700 text-[14px] sm:text-[16px]">
+                        Delicious food, aroma, taste and recipes. <br/>
+                        What are you waiting for?
+                    </p>
+
+                    <button className="bg-accent text-white px-6 py-2 rounded-3xl text-[14px] sm:text-[16px]">
+                        View More
+                    </button>
+
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+
+
+import React from "react";
+import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+    return (
+        <div className="container pt-6">
+            <div className="flex justify-between items-center">
+                <Image src="/logo6.svg" width={50} height={50} alt="logo"/>
+                <ul className="md:flex hidden gap-8 items-center font-semibold text-[14px]">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Recipes</li>
+                    <li>Saved</li>
+                    <li>Profile</li>
+
+                    <button className="bg-accent text-white px-6 py-2 rounded-3xl ">SignUp</button>
+                </ul>
+                <AiOutlineMenu className="md:hidden text-accent" size={30} />
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
+
